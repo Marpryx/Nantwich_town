259a15a30c50c57980850a614f2d8f271bb5074d
Marpryx <pritismar@hotmail.com>
2017-07-05 15:00:05 -0400

    Added JS google map

---
+++ javaScript/nantwich.js
@@ -0,0 +1,80 @@
+// var message;
+// message = 'Hello!';
+
+// alert( message ); // выведет содержимое переменной
+
+
+// var years = prompt('Сколько вам лет?', 100);
+
+// alert('Вам ' + years + ' лет!')
+
+// console.log('TEST console');
+
+
+// var isAdmin = confirm("Вы - администратор?");
+
+// alert( isAdmin );
+
+// var years = prompt('Сколько вам лет?', 3);
+
+// switch (years) {
+//   case '3':
+//     alert( 'Маловато' + years );
+//     break;
+//   case '4':
+//     alert( 'В точку!' + years );
+//     break;
+//   case '5':
+//     alert( 'Перебор' + years);
+//     break;
+//   default:
+//     alert( 'Я таких значений не знаю' + years );
+// }
+
+// function sayHi() {
+//   alert( "Привет" );
+// }
+
+// alert( sayHi );
+
+// function pow(x, n) {
+//   if (n != 1) { // пока n != 1, сводить вычисление pow(x,n) к pow(x,n-1)
+//     return x * pow(x, n - 1);
+//   } else {
+//     return x;
+//   }
+// }
+
+// alert( pow(2, 3) ); // 8
+
+
+// function sum(n){
+
+// 	var add = 0;
+
+// 	for(var i = 1; i <= n; i++){
+// 	    add += i; 
+// 	}
+// 		return add;
+	
+
+// }
+
+//alert( sum(5) );
+
+
+// function sumTo(n) {
+//   if (n == 1) return 1;
+//   return n + sumTo(n - 1);
+// }
+
+// alert( sumTo(5) );
+
+
+// function fib(n){
+// return 
+// }
+
+// alert( fib(3) ); // 2
+// alert( fib(7) ); // 13
+// alert( fib(77)); // 5527939700884757
