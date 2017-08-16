df326754100ddf078e2b46868ff30a196c4ccc25
Marpryx <pritismar@hotmail.com>
2017-08-16 14:53:26 -0400

    Adding CSS modification to 5 days weather forecast

--- javaScript/weather.js
+++ javaScript/weather.js
@@ -4,15 +4,22 @@
 
 
 $(document).ready(function(){
+
   getData();
+
   // getTime();
   getTodayWeather();
   getWeather();
+
 });
 
 function getData(){
   var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-  document.getElementById("currentData").innerHTML = utc;
+  document.getElementById("currentData").innerHTML = "Today is " + utc;
+  $('#currentData').css({"color": "#135A73", "font-size": "150%", "font-weight": "bold", "text-decoration":" underline"});
+ // $('#todayWeather').css({"border": "3px  solid blue"});
+ $('#todayWeather p').css({"margin-bottom": "5px"});
+ $('#humidity').css({"color": "#CD0731"});
 }
 
 function getTime(){
@@ -35,11 +42,12 @@ function getTodayWeather(){
       console.log(result.current_observation.image.url);
 
       document.getElementById("pic").innerHTML = '<img src=' + result.current_observation.icon_url + '>';
-
       document.getElementById("temp").innerHTML =  result.current_observation.temp_c + "&degC";
       document.getElementById("humidity").innerHTML =  result.current_observation.relative_humidity;
       document.getElementById("weather").innerHTML =  result.current_observation.weather;
 
+
+$('#temp').css({"color": "#3E6FC6", "font-size": "100%", "font-weight": "bold"});  // to change CSS properties of an object
 
     }
 
@@ -77,23 +85,13 @@ for( var index = 1; index < 6; index++ ) {
   var temperature = result.forecast.simpleforecast.forecastday[index].high.celsius + "&degC" + "/" + result.forecast.simpleforecast.forecastday[index].low.celsius + "&degC";
   var condition = result.forecast.simpleforecast.forecastday[index].conditions;
   var humidity = result.forecast.simpleforecast.forecastday[index].avehumidity + "%";
-  // document.getElementById("day" + index).innerHTML =  result.forecast.simpleforecast.forecastday[index].date.day;
-
-  // document.getElementById("iconDay" + index).innerHTML = '<img src=' + result.forecast.simpleforecast.forecastday[index].icon_url + '>';
-  // document.getElementById("temperatureDay" + index).innerHTML = result.forecast.simpleforecast.forecastday[index].high.celsius + "&degC" + "/" + result.forecast.simpleforecast.forecastday[index].low.celsius + "&degC";
-  // document.getElementById("conditionDay" + index).innerHTML =  result.forecast.simpleforecast.forecastday[index].conditions;
-  // document.getElementById("humidityDay" + index).innerHTML = result.forecast.simpleforecast.forecastday[index].avehumidity + "%";
 
 
-weatherHTML += '<div class="day"><p id="month1">' + monthName + ',' + ' </p><p id="day1">' + dayNum + '</p><p id="iconDay1">' + pic + '</p><p id="temperatureDay1">' + temperature + '</p><p id="conditionDay1">' + condition + '</p><p id="humidityDay1">' + humidity + '</p> </div>';
+  weatherHTML += '<div class="day"><p id="month1">' + monthName + ',' + ' </p><p id="day1">' + dayNum + '</p><p id="iconDay1">' + pic + '</p><p id="temperatureDay1">' + temperature + '</p><p id="conditionDay1">' + condition + '</p><p id="humidityDay1">' + humidity + '</p> </div>';
 
 }
-
-
 $('#weatherForecast').html(weatherHTML);
-
 }
-
 
 
 });
